f6e5462edacd45f704c9f8f55d0a26008be72708
nikitanovikov <Vertihost@yandex.com>
2023-11-25 23:59:58 +0300

    Добавил статику и некоторые изменения в стилях

--- sber_credit_x/sber_credit_x/settings.py
+++ sber_credit_x/sber_credit_x/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'main',
 ]
 
+ROOT_URLCONF = 'sber_credit_x.urls'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -116,9 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
--- sber_credit_x/sber_credit_x/urls.py
+++ sber_credit_x/sber_credit_x/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('', include('main.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
